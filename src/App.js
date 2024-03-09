97e1b8a36385009115209a3aa0476d339b9da58f
Nikita Van <nikitaszvan@gmail.com>
2024-03-08 21:00:42 -0500

    changed to functional components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,34 @@
 import './App.css';
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component.jsx';
 import SearchBox from './components/search-box/search-box.component.jsx';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ''
-    };
-    // constructor runs first and initializes state
-  }
+const App = () => {
+  const [ searchField, setSearchField ] = useState('');
+  const [ monsters, setMonsters ] = useState([]);
+  const [ filteredMonsters, setFilteredMonsters ] = useState(monsters);
 
-  componentDidMount() {
-    // runs third
-    // componentDidMount is a lifecycle method in React class components that is called after the coponent has been rendered to the DOM
-    // It is common to use for DOM-interaction tasks or fetching data from API
+  useEffect (() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
-    .then((users) =>
-      this.setState(
-        ()=> {
-          return {monsters: users};
-        }, 
-        () => {
-          // console.log(this.state);
-        }
-      )
-    );
-  }
+    .then((users) => setMonsters(users));
+  }, []);
 
-  onSearchChange = (e) => {
-    const searchField = e.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
+  useEffect (() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
+    setFilteredMonsters(newFilteredMonsters);
+  }, [monsters, searchField]);
+
+  const onSearchChange = (e) => {
+    setSearchField(e.target.value.toLocaleLowerCase());
   }
 
- render() {
-  // runs second
-  // React notices component 'monsters' changes after the return of .setStates() first argument
-  // result: Re-render will occur, runs again (fourth)
-  const { monsters, searchField} = this.state;
-  const { onSearchChange } = this;
-  
-  const filteredMonsters = monsters.filter((monster) => {
-    return monster.name.toLocaleLowerCase().includes(searchField);
-  });
-  return(
+  console.log('rendered');
+
+  return (
     <div className='App'>
       <h1 className="app-title">Monsters Rolodex</h1>
       <SearchBox
@@ -59,7 +39,67 @@ class App extends Component {
       <CardList monsters = { filteredMonsters } />
     </div>
   )
- }
 }
 
 export default App;
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: ''
+//     };
+//     // constructor runs first and initializes state
+//   }
+
+//   componentDidMount() {
+//     // runs third
+//     // componentDidMount is a lifecycle method in React class components that is called after the coponent has been rendered to the DOM
+//     // It is common to use for DOM-interaction tasks or fetching data from API
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then((response) => response.json())
+//     .then((users) =>
+//       this.setState(
+//         ()=> {
+//           return {monsters: users};
+//         }, 
+//         () => {
+//           // console.log(this.state);
+//         }
+//       )
+//     );
+//   }
+
+//   onSearchChange = (e) => {
+//     const searchField = e.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//   }
+
+//  render() {
+//   // runs second
+//   // React notices component 'monsters' changes after the return of .setStates() first argument
+//   // result: Re-render will occur, runs again (fourth)
+//   const { monsters, searchField} = this.state;
+//   const { onSearchChange } = this;
+  
+//   const filteredMonsters = monsters.filter((monster) => {
+//     return monster.name.toLocaleLowerCase().includes(searchField);
+//   });
+//   return(
+//     <div className='App'>
+//       <h1 className="app-title">Monsters Rolodex</h1>
+//       <SearchBox
+//         className = 'monster-search-box'
+//         onSearchHandler = { onSearchChange }
+//         placeholder = 'search monsters'
+//       />
+//       <CardList monsters = { filteredMonsters } />
+//     </div>
+//   )
+//  }
+// }
+
+// export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,16 +1,28 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 
-class Card extends Component {
-    render() {
-        const { name, email, id } = this.props;
-        return (
-            <div className="card-container" key={id}>
-                <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={ `monster ${name}` } />
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        )
-    }
+const Card = ( { monster: { name, email, id } } ) => {
+
+    return (
+        <div className="card-container" key={id}>
+            <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={ `monster ${name}` } />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    )
+
 }
+
+// class Card extends Component {
+//     render() {
+//         const { name, email, id } = this.props;
+//         return (
+//             <div className="card-container" key={id}>
+//                 <img src={`https://robohash.org/${id}?set=set2&size=180x180`} alt={ `monster ${name}` } />
+//                 <h2>{name}</h2>
+//                 <p>{email}</p>
+//             </div>
+//         )
+//     }
+// }
 
 export default Card;
